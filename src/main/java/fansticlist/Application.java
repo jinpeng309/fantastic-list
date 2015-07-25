b8754d0274f543b7b5332136d20471acdb16cced
capslock <capslocklts@hotmail.com>
2015-07-25 21:27:21 +0800

    first commit

---
+++ src/main/java/fansticlist/Application.java
@@ -0,0 +1,20 @@
+package fansticlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import fansticlist.interceptors.AccessTokenInterceptor;
+
+@SpringBootApplication
+public class Application extends WebMvcAutoConfigurationAdapter{
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AccessTokenInterceptor()).addPathPatterns("/user/**");
+	}
+}
---
+++ src/main/java/fansticlist/interceptors/AccessTokenInterceptor.java
@@ -0,0 +1,21 @@
+package fansticlist.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AccessTokenInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		String atoken = request.getParameter("atoken");
+		System.out.println("get atoken " + atoken);
+		if (atoken == null || atoken.length() > 0) {
+			return false;
+		}
+		return super.preHandle(request, response, handler);
+	}
+	
+}
